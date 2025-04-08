a11e0e938e0a563804ba597fbd090baa559f3a0c
root <aluno24171@ipt.pt>
2025-04-08 11:54:11 +0000

    o script espera que algo seja adicionado a diretoria /simulation_data antes de fazer o GET request

--- simulation_preproc.py
+++ simulation_preproc.py
@@ -1,28 +1,49 @@
 import requests
 import os
 import subprocess
+import inotify.adapters
 
-response = requests.get('http://172.17.0.3:3000/getFirstInQueue')
+# vai monitorar todos os eventos que ocorren no directoria /simulation_data e às suas subdiretorias
+i = inotify.adapters.InotifyTree('/simulation_data')
 
-if response.status_code == 200:
-    data = response.json()
+while True:
+    # se o evento for NONE (não existir) continua a execução do loop
+    for event in i.event_gen():
+        if event is None:
+            continue
+        # extrai a mask do evento
+        event_mask = event[1]  
 
-    user = (data.get('simulation')).split('_')[0]
-    simulation_dir = "sim_" +(data.get('simulation')).split('_')[1]
+        # se alguma diretoria ou ficheiro for criado então executa o código
+        if 'IN_CREATE' in event_mask in event_mask:
+            # GET request para obter os dados da próxima simulação 
+            response = requests.get('http://172.17.0.3:3000/getFirstInQueue')
+            if response.status_code == 200:
+                data = response.json()
 
+                # extrai o user da resposta
+                user = (data.get('simulation')).split('_')[0]
+                # cria a diretoria com base na resposta 
+                simulation_dir = "sim_" +(data.get('simulation')).split('_')[1]
 
-    simulation_path = os.path.join('/simulation_data',user,simulation_dir)
-    subprocess.run(['python3', "sanitize_and_move_simulations.py", simulation_path]) 
+                # cria a diretoria onde os dados da simulação vão ser guardados
+                simulation_path = os.path.join('/simulation_data',user,simulation_dir)
+                # corre o script que vai tratar e mover os dados da simulação para a diretoria onde ocorre a simulação
+                subprocess.run(['python3', "sanitize_and_move_simulations.py", simulation_path]) 
 
-    subprocess.run(['ros2', 'launch', 'test_env', 'gaden_preproc_launch.py', f'scenario:={user+"_"+simulation_dir}'])
+                # corre o script do gaden para fazer o pre-processamento dos dados da simulação
+                subprocess.run(['ros2', 'launch', 'test_env', 'gaden_preproc_launch.py', f'scenario:={user+"_"+simulation_dir}'])
 
-    updateStatusToInSimulation = requests.post('http://172.17.0.3:3000/setStatusToInSimulation', json={
-        'simulation': data.get('simulation'),
-    })
+                # faz um POST request para atualizar o status da simulação para indicar que já está em simulação
+                updateStatusToInSimulation = requests.post('http://172.17.0.3:3000/setStatusToInSimulation', json={
+                    'simulation': data.get('simulation'),
+                })
 
-    updateStatusToDone = requests.post('http://172.17.0.3:3000/setStatusToDone', json={
-        'simulation': data.get('simulation'),
-    })
-
-else:
-    print(f"Failed to get data. Status code: {response.status_code}")
+                # faz um POST request para atualizar o status da simulação para indicar que já está concluída
+                updateStatusToDone = requests.post('http://172.17.0.3:3000/setStatusToDone', json={
+                    'simulation': data.get('simulation'),
+                })
+            else:
+                print(f"Error")
+        else:
+            continue
